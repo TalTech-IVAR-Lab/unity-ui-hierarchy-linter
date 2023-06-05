2c7230e0cb3a7b22a7d21d4d67e75d22ce78018e
Yevhen Bondarenko <zekbond@gmail.com>
2023-06-05 15:45:49 +0300

    Add option to lint the whole hierarchy from the context menu

--- Editor/HierarchyLinterMenuItems.cs
+++ Editor/HierarchyLinterMenuItems.cs
@@ -1,30 +1,72 @@
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 namespace EE.TalTech.IVAR.UnityUIHierarchyLinter
 {
     internal static class HierarchyLinterMenuItems
     {
-        private const string MenuEntryPath = "GameObject/UI Hierarchy Linter/Lint Selection %&l";
+        private const string BaseMenuEntryPath = "GameObject/UI Hierarchy Linter/";
 
-        [MenuItem(MenuEntryPath, false, 10)]
-        private static void LintHierarchy()
+        #region Selection
+
+        private const string LintSelectionEntryPath = BaseMenuEntryPath + "Lint Selection %&l";
+
+        [MenuItem(LintSelectionEntryPath, false, 10)]
+        private static void LintSelection()
         {
             var selectedGameObject = Selection.activeGameObject;
 
-            Undo.RegisterFullObjectHierarchyUndo(selectedGameObject, "Lint UI hierarchy");
+            Undo.RegisterFullObjectHierarchyUndo(selectedGameObject, "Lint UI hierarchy for selection");
             HierarchyLinterCore.RunLinters(selectedGameObject);
 
             Debug.Log($"Linted UI hierarchy of {selectedGameObject.name}.", selectedGameObject);
         }
 
-        [MenuItem(MenuEntryPath, true)]
-        private static bool ValidateLintHierarchy()
+        [MenuItem(LintSelectionEntryPath, true)]
+        private static bool ValidateLintSelection()
         {
             if (Selection.activeGameObject == null) return false;
             if (Selection.activeGameObject.GetComponent<RectTransform>() == null) return false;
 
             return HierarchyLinterCore.CanLint();
         }
+
+        #endregion
+
+        #region Whole
+
+        private const string LintWholeEntryPath = BaseMenuEntryPath + "Lint Whole Hierarchy %&#l";
+
+        [MenuItem(LintWholeEntryPath, false, 10)]
+        private static void LintWholeHierarchy()
+        {
+            Undo.IncrementCurrentGroup();
+
+            // register undo for all root objects containing RectTransforms
+            var rects = StageUtility.GetCurrentStageHandle().FindComponentsOfType<RectTransform>();
+            for (int i = 0; i < rects.Length; i++)
+            {
+                var rect = rects[i];
+                Undo.RegisterCompleteObjectUndo(
+                    rect.gameObject,
+                    $"Lint whole UI hierarchy (rect {i + 1} out of {rects.Length}, '{rect.name}')"
+                );
+            }
+
+            HierarchyLinterCore.RunLinters();
+
+            Undo.SetCurrentGroupName("Lint whole UI hierarchy");
+
+            Debug.Log($"Linted whole UI hierarchy in the current scene.");
+        }
+
+        [MenuItem(LintWholeEntryPath, true)]
+        private static bool ValidateLintHierarchy()
+        {
+            return HierarchyLinterCore.CanLint();
+        }
+
+        #endregion
     }
 }
